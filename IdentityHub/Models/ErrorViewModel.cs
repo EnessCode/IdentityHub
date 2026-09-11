70e77fe4820434d1d101d598bacba3529b88278b
Enes <onerenes12@gmail.com>
2026-09-11 12:18:17 +0300

    Add project files.

---
+++ IdentityHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
