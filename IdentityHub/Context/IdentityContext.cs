28e474317f4b81ebf8c606ce189e88641bf2f7b0
Enes <onerenes12@gmail.com>
2026-09-11 12:35:16 +0300

    Initial project setup: Configure IdentityContext, AppUser, database migrations, and basic program pipeline

---
+++ IdentityHub/Context/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using IdentityHub.Entities;
+
+namespace IdentityHub.Context
+{
+	public class IdentityContext : IdentityDbContext<AppUser>
+	{
+		public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+		{
+		}
+	}
+}
--- IdentityHub/Program.cs
+++ IdentityHub/Program.cs
@@ -1,7 +1,10 @@
+using IdentityHub.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<IdentityContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
