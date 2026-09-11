6d64e9ad65f8ecd1fff610e9534d428c39e038bc
Enes <onerenes12@gmail.com>
2026-09-11 14:01:53 +0300

    Implement user login feature: Add LoginController, LoginViewModel, cookie authentication via SignInManager, and login UI assets

---
+++ IdentityHub/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using IdentityHub.Entities;
+using IdentityHub.Models.User;
+
+namespace IdentityHub.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly SignInManager<AppUser> _signInManager;
+
+		public LoginController(SignInManager<AppUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, true, true);
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı.");
+			}
+			return View(model);
+		}
+	}
+}
---
+++ IdentityHub/Models/User/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityHub.Models.User
+{
+	public class LoginViewModel
+	{
+		[Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz.")]
+		public string Username { get; set; }
+
+		[Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+		public string Password { get; set; }
+	}
+}
